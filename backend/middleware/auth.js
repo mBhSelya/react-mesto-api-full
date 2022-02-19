fd83064c07510ec2965924ef2b1417ec30055252
Aleksey Selyakov <selyakoff18@gmail.com>
2022-02-19 18:35:12 +0300

    feat: Собрать проект!

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
-
-const { JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/unauthorized-error');
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
+  const { JWT_SECRET } = process.env;
 
   try {
     payload = jwt.verify(token, JWT_SECRET);
