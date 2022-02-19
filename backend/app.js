359dd543fb67f5c5960c095a2ee89b2d8da172fb
Aleksey Selyakov <selyakoff18@gmail.com>
2022-02-19 14:22:45 +0300

    feat: Собрать проект!

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).default('Жак-Ив Кусто'),
     about: Joi.string().min(2).max(30).default('Исследователь'),
-    avatar: Joi.string().pattern(urlPattern).default('https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png'),
+    avatar: Joi.string().pattern(urlPattern),
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
   }),
